33a57489e6abbcdcbf408a07d313c36ec59b324a
Craig Dennis <craigsdennis@gmail.com>
2024-07-02 10:05:39 -0400

    Adds LoRA not supported

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,26 @@ app.get('/', async (c) => {
 	// TODO: make this generate automatically
 	return c.html(`
 		<h1>AI Error Explorer</h1>
-		<a href="/no-such-model">No Such Model</a>
+		<ul>
+			<li><a href="/no-such-model">No Such Model</a></li>
+			<li><a href="/lora-not-supported">LoRA not supported</a></li>
+		</ul>
 	`);
 });
+
+app.get('/lora-not-supported', async (c) => {
+	let response;
+	try {
+		response = await c.env.AI.run('@cf/meta/llama-3-8b-instruct', {
+			prompt: 'This model does not support loras',
+			lora: 'jklol'
+		});
+	} catch (err) {
+		return c.json({ err, t: typeof err});
+	}
+	return c.json({ success: true, response });
+});
+
 
 app.get('/no-such-model', async (c) => {
 	let response;
